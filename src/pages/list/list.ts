be511f0824039e41efb660966cc05640b60418a8
Nikhil Kumar <nikhil.y.kumar@pwc.com>
2018-01-16 18:28:46 +0530

    Updating collision function

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -294,17 +294,17 @@ export class ListPage {
     //console.log(event);
   }
 
-  lineIntersects = (p0_x, p0_y, p1_x, p1_y, p2_x, p2_y, p3_x, p3_y) => {
+  lineIntersects = (x1, y1, x2, y2, x3, y3, x4, y4) => {
     
         var s1_x, s1_y, s2_x, s2_y;
-        s1_x = p1_x - p0_x;
-        s1_y = p1_y - p0_y;
-        s2_x = p3_x - p2_x;
-        s2_y = p3_y - p2_y;
+        s1_x = x2 - x1;
+        s1_y = y2 - y1;
+        s2_x = x4 - x3;
+        s2_y = y4 - y3;
     
         var s, t;
-        s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-        t = ( s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+        s = (-s1_y * (x1 - x3) + s1_x * (y1 - y3)) / (-s2_x * s1_y + s1_x * s2_y);
+        t = ( s2_x * (y1 - y3) - s2_y * (x1 - x3)) / (-s2_x * s1_y + s1_x * s2_y);
     
         if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
             this.collosionCount++;
